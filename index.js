d8b3cea7adb303b162098eff92caed3e6cf0e861
Clark Williams <theclarkwilliams@gmail.com>
2019-08-22 14:13:56 -0600

    server up

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const server = require('./server.js');
+
+const port = process.env.PORT || 7000;
+server.listen(port, () => {
+    console.log(`\n** server running on port ${port} **\n`);
+})
